d77db8db9fc1dc2267f779810a24b0ee435f93f7
Rishabh <Daredevil@Rishabhs-MacBook-Air.local>
2017-07-08 23:49:25 +0530

    using environmental variables

--- app.js
+++ app.js
@@ -16,8 +16,9 @@ var commentRoutes    = require("./routes/comments"),
 	campgroundRoutes = require("./routes/campgrounds"),
 	indexRoutes	     = require("./routes/index")	
 
-//mongoose.connect("mongodb://localhost/yelp_camp");
-mongoose.connect("mongodb://daredevil:daredevil47@ds151702.mlab.com:51702/yelpcamp_47");
+var url = process.env.DATABASEURL || "mongodb://localhost/yelp_camp"
+mongoose.connect(url);
+
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine","ejs");
 app.use(express.static(__dirname + "/public"));
